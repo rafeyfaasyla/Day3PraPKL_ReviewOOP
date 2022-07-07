253e0fc991e1a20fec4706d8ae3b77f7ce32f694
rafeyfaasyla <asyla_039@smkassalaambandung.sch.id>
2022-07-07 16:02:54 +0700

    Ganti nama guru

--- guru/index.php
+++ guru/index.php
@@ -21,7 +21,7 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item active">
-                <a class="nav-link" href="index.php">dosen <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php">guru <span class="sr-only">(current)</span></a>
             </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
